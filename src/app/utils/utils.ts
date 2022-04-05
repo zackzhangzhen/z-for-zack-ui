8920d0ac1b606511086a119afd501c6d7dba29b2
Zack Zhang <Zack.Zhang@dell.com>
2022-04-05 19:29:07 +0800

    [Feature] Added Sorting By Date For Blogs
    Get blogs sorted by date in desc order.

--- src/app/utils/utils.ts
+++ src/app/utils/utils.ts
@@ -1,7 +1,17 @@
-export function toDateString(date: Date): string {
-  if (date) {
-   return new Date(date).toDateString();
-  }
+import {DatePipe} from "@angular/common";
 
+export function toDateString(date: Date): string {
+  try {
+    if (date) {
+      const datePipe: DatePipe = new DatePipe('en-US')
+      let dateString = datePipe.transform(date, 'dd-MMM-YYYY HH:mm:ss a')
+
+      if (!!dateString) {
+        return dateString;
+      }
+    }
+  } catch (error) {
+    console.log(error);
+  }
   return "Not Dated";
 }
